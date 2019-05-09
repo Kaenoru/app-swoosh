b68520a17a45183a12bf90db8abf51a64cba907c
Maksim Okala-Kulak <kaenoru@192.168.100.4>
2019-05-10 01:38:39 +0300

    new files structure and View Controllers

---
+++ app-swoosh/Controller/LeagueVC.swift
@@ -0,0 +1,30 @@
+//
+//  LeagueVC.swift
+//  app-swoosh
+//
+//  Created by Maksim Okala-Kulak on 5/10/19.
+//  Copyright © 2019 Maksim Okala-Kulak. All rights reserved.
+//
+
+import UIKit
+
+class LeagueVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
