b47c0812129d930955203cce9c1a68f431aeea00
Maksim Okala-Kulak <kaenoru@192.168.100.4>
2019-05-10 01:19:33 +0300

    constrains set up

--- app-swoosh/ViewController.swift
+++ app-swoosh/ViewController.swift
@@ -13,5 +13,15 @@ class ViewController: UIViewController {
     @IBOutlet weak var swoosh: UIImageView!
     @IBOutlet weak var bgImg: UIImageView!
 
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
+        
+        
+    }
+    
+    
 }
 
