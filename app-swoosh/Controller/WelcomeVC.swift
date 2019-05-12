c96799ad3ce63252faaa9d48788ff23c8f6d2798
Maksim Okala-Kulak <kaenoru@192.168.100.4>
2019-05-12 23:08:41 +0300

    app finished and tested

--- app-swoosh/Controller/WelcomeVC.swift
+++ app-swoosh/Controller/WelcomeVC.swift
@@ -9,18 +9,14 @@
 import UIKit
 
 class WelcomeVC: UIViewController {
-    
-    @IBOutlet weak var swoosh: UIImageView!
-    @IBOutlet weak var bgImg: UIImageView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBAction func unwindFromSkillVC(unwindSegue: UIStoryboardSegue) {
-        
-        
-    }
+    @IBAction func unwindFromLeagueVC(unwindSegue: UIStoryboardSegue) {
+        }
     
     
 }
---
+++ app-swoosh/Model/player.swift
@@ -0,0 +1,15 @@
+//
+//  Player.swift
+//  app-swoosh
+//
+//  Created by Maksim Okala-Kulak on 5/10/19.
+//  Copyright © 2019 Maksim Okala-Kulak. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkillLevel: String!
+    
+}
